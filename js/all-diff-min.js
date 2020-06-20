b4c24015f539229e8fb867cff5b3dd43b73e6342
sushant0789 <53333023+sushant0789@users.noreply.github.com>
2020-06-20 16:39:43 +0530

    camera access

--- js/all-diff-min.js
+++ js/all-diff-min.js
@@ -81,3 +81,35 @@ $(document).on("click", ".input-button-next", function () {
     }),
       i();
   });
+
+("use strict");
+
+const video = document.getElementById("video");
+const errorMsgElement = document.querySelector("span#errorMsg");
+
+const constraints = {
+  audio: true,
+  video: {
+    width: 1280,
+    height: 720,
+  },
+};
+
+// Access webcam
+async function init() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+
+// Success
+function handleSuccess(stream) {
+  window.stream = stream;
+  video.srcObject = stream;
+}
+
+// Load init
+init();
